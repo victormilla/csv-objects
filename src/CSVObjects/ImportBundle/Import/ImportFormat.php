afdc96cb198ad6fb67e07afe27a977c171eca9ed
Alejandro Hernandez <alejandrohs@gmail.com>
2016-08-20 19:22:24 +0200

    Added assessment importer service Added assessment import file form type as a service Created assessment importer service to read the existing formats and parse the file contents according to them (Cherry-picked from another project)

---
+++ src/CSVObjects/ImportBundle/Import/ImportFormat.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace CSVObjects\ImportBundle\Import;
+
+use Symfony\Component\HttpFoundation\File\File;
+
+class ImportFormat
+{
+    const SCHOOL_FROM_ID = 'Name';
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string[]
+     */
+    private $columns;
+
+    /**
+     * @var string
+     */
+    private $schoolColumn;
+
+    /**
+     * @var string
+     */
+    private $schoolSearchBy;
+
+    /**
+     * @var string
+     */
+
+    private $studentColumn;
+
+    /**
+     * @var string
+     */
+    private $studentSearchBy;
+
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var FullResultId[]
+     */
+    private $assessments;
+
+    public function __construct($id, $name, $columns, $schoolColumn, $schoolSearchBy, $studentColumn, $studentSearchBy, $assessments)
+    {
+        $this->id              = $id;
+        $this->name            = $name;
+        $this->columns         = array_flip($columns);
+        $this->schoolColumn    = $schoolColumn;
+        $this->schoolSearchBy  = $schoolSearchBy;
+        $this->studentColumn   = $studentColumn;
+        $this->studentSearchBy = $studentSearchBy;
+        $this->assessments     = $assessments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param File $file
+     *
+     * @return array
+     */
+    public function extractResultsFromFile(File $file)
+    {
+        $data = $this->readFile($file);
+        $this->validate($data);
+
+        $header  = array_flip($data[0]);
+        $results = array();
+        for ($i = 1; count($data); $i++) {
+            $results[] = $this->createResult($header, $data[$i]);
+        }
+
+        return $results;
+    }
+
+    /**
+     * @param File $file
+     *
+     * @return string[]
+     */
+    private function readFile(File $file)
+    {
+        $data = array();
+        if ('xlsx' === $file->guessExtension()) {
+            // @TODO read excel to array
+        } else {
+            $fileHandle = fopen($file->getPathname(), 'r');
+            $data       = array();
+            while (!feof($fileHandle)) {
+                $data[] = fgetcsv($fileHandle);
+            }
+            fclose($fileHandle);
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param string[] $data
+     */
+    private function validate($data)
+    {
+        if (!is_array($data) || !isset($data[0])) {
+            throw new \InvalidArgumentException('File does not have CSV content');
+        }
+
+        if (count($data[0]) !== count($this->columns)) {
+            throw new \InvalidArgumentException('File does not have the required number of columns');
+        }
+
+        foreach ($data[0] as $column) {
+            if (!isset($this->columns[$column])) {
+                throw new \InvalidArgumentException('File contains an unrecognised column ' . $column);
+            }
+        }
+    }
+
+    /**
+     * @param string[] $header
+     * @param string[] $row
+     *
+     * @return FullResult
+     */
+    private function createResult($header, $row)
+    {
+        // @TODO return parsed row
+
+        return null;
+    }
+}
